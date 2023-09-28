fce2ad7a13785532de8c4b5ed7561d9aac075f7c
Zaurbek Shapiev <shapiev.zaur@outlook.com>
2023-09-28 12:24:38 +0800

    Added cart slice and implemented its functionality

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     "bg-yellow-400 text-sm focus:ring-offset inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed focus:transition focus:duration-300";
   const styles = {
@@ -9,6 +9,13 @@ function Button({ children, disabled, to, type }) {
     secondary:
       "focus:ring-offset inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:text-stone-800 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 hover:text-stone-800 disabled:cursor-not-allowed focus:transition focus:duration-300 px-4 py-2.5 md:px-6 md:py-3.5 ",
   };
+  if (onClick) {
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+  }
   if (to)
     return (
       <Link className={styles[type]} to={to}>
